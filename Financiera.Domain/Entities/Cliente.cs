7fc23cd4693991bd310e1c319d81013b1d257307
Mauricio <cr.maury@hotmail.com>
2020-02-24 02:36:41 -0300

    first commit

---
+++ Financiera.Domain/Entities/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.Domain.Entities
+{
+    public class Cliente
+    {
+        #region Properties
+        public int idCliente { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string domicilio { get; set; }
+        public string telefono { get; set; }
+        public double sueldo { get; set; }
+        #endregion
+
+        #region Constructors
+        public Cliente()
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+        public void MyMethod()
+        {
+
+        }
+        #endregion        
+    }
+}
